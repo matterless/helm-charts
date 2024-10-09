baeaba0e7e8cb0381768c87a4ccf28794cd4ee7f
Oskar Levin <oskar@matterless.com>
2024-10-09 16:49:18 +0800

    fix: switch to aws cli to not expose password in actions output

--- charts/cactus-backend/scripts/pre-install.sh
+++ charts/cactus-backend/scripts/pre-install.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
-: "${DOCKER_PASSWORD?Need to set DOCKER_PASSWORD}"
+registry="992382468184.dkr.ecr.eu-central-1.amazonaws.com"
+username="AWS"
+password="$(aws ecr get-login-password | docker login --username "$username" --password-stdin "$registry")"
 
 kubectl create secret \
   docker-registry \
   matterless-common-ecr-credentials \
-  --docker-server="992382468184.dkr.ecr.eu-central-1.amazonaws.com" \
-  --docker-username="AWS" \
-  --docker-password="$DOCKER_PASSWORD"
+  --docker-server="$registry" \
+  --docker-username="$username" \
+  --docker-password="$password"
 
 yq -i '.imagePullSecrets[0].name = "matterless-common-ecr-credentials"' "$(dirname "$0")/../values.yaml"

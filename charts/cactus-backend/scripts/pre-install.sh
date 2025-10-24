83d1c2ed07aa3ec5a2341b6cab78d7b3be55725a
Shuning <shuning@aukilabs.com>
2025-10-24 12:05:07 +0800

    fix secret

--- charts/cactus-backend/scripts/pre-install.sh
+++ charts/cactus-backend/scripts/pre-install.sh
@@ -13,4 +13,12 @@ kubectl create secret \
   --dry-run=client \
   -o yaml > "$(dirname "$0")/../templates/ecr-testing-secret.yaml"
 
+kubectl create secret generic cactus-backend-test \
+  --from-literal=AUTH_JWT_PROFILE=$AUTH_JWT_PROFILE \
+  --from-literal=APP_SECRET=test \
+  --from-literal=TOKEN_ENCRYPTION_KEY=test \
+  --from-literal=REDIS_PASSWORD=cactus-backend \
+  --from-literal=POSTGRES_PASSWORD=cactus-backend \
+  --dry-run=client -o yaml | kubectl apply -f -
+
 yq -i '.imagePullSecrets[0].name = "matterless-common-ecr-credentials"' "$(dirname "$0")/../values.yaml"

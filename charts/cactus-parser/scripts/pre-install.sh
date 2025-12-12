0fccbd98131427b39865b5f46b1a5ee90fb2517c
Shuning <shuning@aukilabs.com>
2025-12-12 15:17:49 +0800

    added pg host and redis addrs

--- charts/cactus-parser/scripts/pre-install.sh
+++ charts/cactus-parser/scripts/pre-install.sh
@@ -16,3 +16,5 @@ kubectl create secret \
 yq -i '.imagePullSecrets[0].name = "matterless-common-ecr-credentials"' "$(dirname "$0")/../values.yaml"
 yq -i '.image.repository = "992382468184.dkr.ecr.eu-central-1.amazonaws.com/cactus-pidb"' "$(dirname "$0")/../values.yaml"
 yq -i '.image.tag = "feature-integration-api-cleanup"' "$(dirname "$0")/../values.yaml"
+yq -i '.envVars.REDIS_MASTER_ADDR = "redis-master:6379/0"' "$(dirname "$0")/../values.yaml"
+yq -i '.envVars.REDIS_REPLICA_ADDR = "redis-replicas:6379/0"' "$(dirname "$0")/../values.yaml"

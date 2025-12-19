842d7fa83e903e78568bd69bb5211cda06f238c1
Shuning <shuning@aukilabs.com>
2025-12-19 12:17:57 +0800

    add events env vars

--- charts/cactus-backend/scripts/pre-install.sh
+++ charts/cactus-backend/scripts/pre-install.sh
@@ -18,3 +18,4 @@ yq -i '.secretData.AUTH_JWT_PROFILE = strenv(AUTH_JWT_PROFILE)' "$(dirname "$0")
 yq -i '.secretData.APP_SECRET = strenv(APP_SECRET)' "$(dirname "$0")/../values.yaml"
 yq -i '.envVars.APP_KEY = strenv(APP_KEY)' "$(dirname "$0")/../values.yaml"
 yq -i '.image.tag = "feature-integration-api-cleanup"' "$(dirname "$0")/../values.yaml"
+yq -i '.image.repository = "992382468184.dkr.ecr.eu-central-1.amazonaws.com/internal-cactus-backend"' "$(dirname "$0")/../values.yaml"

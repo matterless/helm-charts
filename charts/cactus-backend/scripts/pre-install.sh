cb27d0af318c743413adf43866b0ccccdf60bb40
Shuning <shuning@aukilabs.com>
2025-12-19 12:03:28 +0800

    use tag feature-integration-api-cleanup

--- charts/cactus-backend/scripts/pre-install.sh
+++ charts/cactus-backend/scripts/pre-install.sh
@@ -17,4 +17,4 @@ yq -i '.imagePullSecrets[0].name = "matterless-common-ecr-credentials"' "$(dirna
 yq -i '.secretData.AUTH_JWT_PROFILE = strenv(AUTH_JWT_PROFILE)' "$(dirname "$0")/../values.yaml"
 yq -i '.secretData.APP_SECRET = strenv(APP_SECRET)' "$(dirname "$0")/../values.yaml"
 yq -i '.envVars.APP_KEY = strenv(APP_KEY)' "$(dirname "$0")/../values.yaml"
-
+yq -i '.image.tag = "feature-integration-api-cleanup"' "$(dirname "$0")/../values.yaml"
